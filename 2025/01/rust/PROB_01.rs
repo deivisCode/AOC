8963a754d7bcd627b1de993a5ffd4fb42fc5b1e7
deivis <david.cotelo.online+github@gmail.com>
2026-09-09 18:39:59 +0200

    novo(25/01/rust); programa en rust

---
+++ 2025/01/rust/PROB_01.rs
@@ -0,0 +1,53 @@
+#![allow(unused)]
+
+use std::fs;
+
+#[derive(PartialEq)]
+enum Dir { L, R }
+
+struct Rotacion {
+    dir: Dir,
+    clicks: i32
+}
+
+fn main()
+{
+    let rotacions: String = fs::read_to_string("input.txt")
+        .expect("Non se puido ler");
+
+    let mut novo: i32 = 50;
+    let mut pases: i32 = 0;
+    let mut d_0: i32 = 0;
+
+    for linha in rotacions.lines()
+    {
+
+        let rot = Rotacion
+        {
+            dir    : if linha[0..1] == *"L" { Dir::L } else { Dir::R },
+            clicks : linha[1..].parse::<i32>().unwrap(),
+        };
+
+        if rot.dir == Dir::L
+        {
+            d_0 = if novo != 0 { novo } else { 100 };
+            novo = (novo + (100 - (rot.clicks % 100))) % 100;
+            if (rot.clicks >= d_0)
+            {
+                pases = pases + 1 + ((rot.clicks - d_0) / 100);
+            }
+        }
+        else if rot.dir == Dir::R
+        {
+            d_0 = (100 - novo);
+            novo = (novo + rot.clicks) % 100;
+            if (rot.clicks >= d_0) {
+                pases = pases + 1 + ((rot.clicks - d_0) / 100);
+            }
+        };
+
+    }
+
+    println!("RESULTADO {}", pases);
+
+}
